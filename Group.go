cff347ae391c49f7c9ba5209455e0608f0547db2
Logan Reuter <loganr@kvalinc.com>
2021-08-24 13:28:29 -0700

    minor changes

--- Group.go
+++ Group.go
@@ -3,14 +3,17 @@ package rush
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/fs"
 	"log"
+	"net/http"
 	"os"
 	"path"
+	"reflect"
 	"sync"
 	"time"
 
-	"github.com/lreuter2020/rush/Errors"
+	CError "github.com/lreuter2020/rush/Errors"
 )
 
 type group struct {
@@ -78,4 +81,53 @@ func (g *group) GetAll(v interface{}) {
 	}
 	elapsed := time.Since(start)
 	fmt.Println(elapsed)
+}
+
+func (g *group) PipeAll(w http.ResponseWriter) {
+	var wg sync.WaitGroup
+	pr, pw := io.Pipe()
+	files, err := os.ReadDir(g.path)
+	if err != nil {
+		CError.Emit(err)
+	}
+
+	for _, file := range files {
+
+		if file.IsDir() {
+			wg.Add(1)
+			go func(file fs.DirEntry) {
+				defer wg.Done()
+				buff, err := os.ReadFile(path.Join(g.path, file.Name(), file.Name()+".json"))
+				if err != nil {
+					CError.Emit(err)
+				} else {
+
+					var rv map[string]interface{}
+
+					if err := json.Unmarshal(buff, &rv); err != nil {
+						CError.Emit(err)
+					}
+					json.NewEncoder(pw).Encode(&rv)
+				}
+			}(file)
+		}
+	}
+
+	go func(wg *sync.WaitGroup, pw *io.PipeWriter) {
+		wg.Wait()
+		pw.Close()
+	}(&wg, pw)
+
+	io.Copy(w, pr)
+}
+
+func (g *group) Where(args ...string) {
+
+}
+
+func (g *group) First(args ...interface{}) {
+	if len(args) != 0 && reflect.TypeOf(args[0]).Kind() != reflect.Ptr {
+		panic("First argument must be a pointer")
+	}
+
 }
--- README.md
+++ README.md
@@ -1,3 +1,69 @@
 # Rush
-## NoSQL Database
-#### Based on Google Firebase's Firestore
+## NoSQL Database Written Purely in Go
+
+## Installation
+```bash
+go get github.com/lreuter2020/Rush
+```
+or 
+
+Add the library to import statement and run
+```bash
+go mod tidy
+```
+
+## Usage
+### Create a database connection:
+```go
+func main(){
+    /* 
+        Creates a connection to database.
+        First argument is relative path
+        Second argument is the name of the database
+    */
+    conn, err := rush.Connect("/", "db")
+    if err != nil {
+        panic(err)
+    }
+
+    
+
+    /*
+    
+    */
+    users.Member("Guest").Create(map[string]interface{}{
+        "Name": "Guest",
+        "Age": 18
+    })
+}
+```
+
+### Create A New Group
+_(Equivalent of creating a table in SQL)_
+```go
+    ...
+    
+    /*
+        Creates a collection titled Users.
+    */
+    users := conn.Group("Users")
+    
+    ...
+```
+
+### Create A New Member
+```go
+    ...
+
+    /*
+        Method 1:
+    */
+    users.Member("Guest")
+
+    /*
+        Method 2:
+    */
+    conn.Group("Users").Member("Guest")
+
+    ...
+```
